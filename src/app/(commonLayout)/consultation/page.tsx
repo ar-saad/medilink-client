9defdc678a74d2d3c74ef1250909a2460fe2f939
ar-saad <arsaad.dev@gmail.com>
2026-04-03 09:34:08 +0600

    feat: integrate React Query for data fetching and add QueryProvider component

--- src/app/(commonLayout)/consultation/page.tsx
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -1,5 +1,26 @@
-const ConsultationPage = () => {
-  return <div>Consultation Page</div>;
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getDoctors } from "./_actions";
+import DoctorList from "@/components/modules/Consultation/DoctorList";
+
+const ConsultationPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["doctors"],
+    queryFn: getDoctors,
+  });
+
+  return (
+    <div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <DoctorList />
+      </HydrationBoundary>
+    </div>
+  );
 };
 
 export default ConsultationPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import QueryProvider from "@/providers/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -39,9 +40,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-full flex flex-col">
-        Root Layout
-        <br />
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
